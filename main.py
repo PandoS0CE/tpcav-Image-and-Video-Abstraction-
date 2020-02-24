e8e8ba75c710d64bbe5f7c5ab6b2691ba39a9a29
Tugdual LP <tugduu@gmail.com>
2020-02-24 14:58:10 +0100

    Final version

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import timeit
-import lic
 
 def normalize(v:np.array):
     return v / (np.linalg.norm(v) + 0.0000001)
@@ -24,10 +23,6 @@ def print_debug(array: np.ndarray):
 def structure_tensor_calculation(image_input):
 
     image_input = image_input.astype(float)
-
-    # print(type(image_input[0][0]))
-
-    # print_debug(image_input)
 
     height,width = image_input.shape
     # compute rotational symmetric derivative filter
@@ -52,17 +47,28 @@ def structure_tensor_calculation(image_input):
             G[i][j] = fy[i][j] * fy[i][j]
 
     S = [E,F,G]
-
-    # print_debug(fx)
-    # print_debug(fy)
-
-    # print_debug(E)
-    # print_debug(F)
-    # print_debug(G)
-
+    
     return S
 
+def merge(S1, S2):
+    # Merge 2 structures tensor
+    E1 = S1[0]
+    F1 = S1[1]
+    G1 = S1[2]
+
+    E2 = S2[0]
+    F2 = S2[1]
+    G2 = S2[2]
+
+    result = S1.copy()
+    result[0] = (E1 + E2) / 2.0
+    result[1] = (F1 + F2) / 2.0
+    result[2] = (G1 + G2) / 2.0
+
+    return result
+
 def structure_tensor_smoothing(S):
+    # Apply Gaussian kernel on sturcture tensor
     matrice = np.array([[1.0, 2.0, 1.0], [2.0, 4.0, 2.0], [1.0, 2.0, 1.0]], float)
     kernel = 1.0/16.0 * matrice
 
@@ -93,12 +99,6 @@ def compute_eigenvalue(S):
             lambda1[i][j] = (coeff1[i][j] + np.sqrt(determinant[i][j])) / 2.0
             lambda2[i][j] = (coeff1[i][j] - np.sqrt(determinant[i][j])) / 2.0
 
-    # print_debug(coeff1)
-    # print_debug(determinant)
-
-    # print_debug(lambda1)
-    # print_debug(lambda2)
-
     return lambda1, lambda2
 
 def compute_eigenvector(S, lambda1, lambda2):
@@ -118,17 +118,6 @@ def compute_eigenvector(S, lambda1, lambda2):
 
     return eta, xi
 
-def add_padding(img,size):
-    borderType = cv2.BORDER_REPLICATE
-
-    top = int(size)
-    bottom = top
-    left = int(size)
-    right = left
-
-    img = cv2.copyMakeBorder(img, top, bottom, left, right, borderType, None, None)
-    return img
-
 def euler_integration(img, xi, a_sigma):
     img_result = np.zeros(img.shape)
     height, width,_ = img.shape
@@ -136,7 +125,6 @@ def euler_integration(img, xi, a_sigma):
         for j in range(width):
             # compute l
             l = math.ceil(2*a_sigma[i][j])
-            # print("l = " + str(l))
 
             # compute each t_k and x_k
             t_minus = []
@@ -154,63 +142,11 @@ def euler_integration(img, xi, a_sigma):
                 t_plus.append(np.sign(np.dot(t_plus[k-1],xi[x_plus[k-1][0]][x_plus[k-1][1]])) * xi[x_plus[k-1][0]][x_plus[k-1][1]])
                 x_minus.append(np.add(x_minus[k-1], t_minus[k-1]).astype("int"))
                 x_plus.append(np.add(x_plus[k-1], t_plus[k-1]).astype("int"))
+
             np.asarray(x_minus)
             np.asarray(x_plus)
             
             # create 1 dimensional gaussian with a_sigma
-            kernel = cv2.getGaussianKernel( 2*l+1, a_sigma[i][j])
-            # apply the gaussian kernel with the x_k
-            v_result = kernel[l]*img[x_minus[0][0]][x_minus[0][1]]
-            for k in range(1,l):
-                v_result = v_result + kernel[l+k]* img[x_plus[k][0]][x_plus[k][1]]
-                v_result = v_result + kernel[l-k]* img[x_minus[k][0]][x_minus[k][1]]
-            # v_result = v_result / np.sum(cv2.getGaussianKernel( 2 * l[i_pad][j_pad] + 1, a_sigma[i_pad][j_pad]))
-            img_result[i][j] = v_result
-      
-    return img_result
-
-def euler_integration_test(img, xi, a_sigma):
-    img_result = np.zeros(img.shape)
-    height, width,_ = img.shape
-    for i in range(height):
-        for j in range(width):
-            # compute l
-            l = math.ceil(2*a_sigma[i][j])
-            # print("l = " + str(l))
-
-            # compute each t_k and x_k
-            t_minus = []
-            t_plus = []
-            t_minus.append(-xi[i][j])
-            t_plus.append(xi[i][j])
-            x_minus = []
-            x_plus = []
-            x_minus.append(np.array([i,j],dtype=int))
-            x_plus.append(np.array([i,j],dtype=int))
-
-            for k in range(1,l+1):
-                # sign<t_k-1,xi> * xi
-                t_minus.append(np.sign(np.dot(t_minus[k-1],xi[x_minus[k-1][0]][x_minus[k-1][1]])) * xi[x_minus[k-1][0]][x_minus[k-1][1]])
-                t_plus.append(np.sign(np.dot(t_plus[k-1],xi[x_plus[k-1][0]][x_plus[k-1][1]])) * xi[x_plus[k-1][0]][x_plus[k-1][1]])
-                x_minus.append(np.add(x_minus[k-1], t_minus[k-1]).astype("int"))
-                x_plus.append(np.add(x_plus[k-1], t_plus[k-1]).astype("int"))
-
-            np.asarray(t_minus)
-            np.asarray(t_plus)
-            np.asarray(x_minus)
-            np.asarray(x_plus)
-
-            # print(str(i) + ", " + str(j))
-            # print("t_plus : " + str(t_plus))
-            # print("x_plus : " + str(x_plus))
-
-            # if(i % 10 == 0 and j % 10 == 0):
-            #     img[x_plus[0][0]][x_plus[0][1]] = (255,0,0)
-            #     for k in range(1,l+1):
-            #         img[x_plus[k][0]][x_plus[k][1]] = (255,0,0)
-            #         img[x_minus[k][0]][x_minus[k][1]] = (255,0,0)
-            
-            ## create 1 dimensional gaussian with a_sigma
             kernel = cv2.getGaussianKernel( 2*l+1, a_sigma[i][j])
             # apply the gaussian kernel with the x_k
             v_result = kernel[l]*img[x_minus[0][0]][x_minus[0][1]]
@@ -221,7 +157,7 @@ def euler_integration_test(img, xi, a_sigma):
       
     return img_result
 
-def adaptive_smoothing(img_input,lambda1,lambda2,xi,S,n = 1000.0,sigma = 6.0):
+def adaptive_smoothing(img_input,lambda1,lambda2,xi,n = 1000.0,sigma = 6.0):
     # compute the adaptive gaussian sigma
     A = np.zeros(lambda1.shape)
     adapted_sigma = np.zeros(lambda1.shape)
@@ -239,18 +175,12 @@ def adaptive_smoothing(img_input,lambda1,lambda2,xi,S,n = 1000.0,sigma = 6.0):
     # euler integration
     img_input = img_input.astype(float)
 
-    # k_max = 5
-    # for k in range(0,k_max):
-    #     # img_input = euler_integration(img_input, xi, adapted_sigma)
-    #     img_input = euler_integration_test(img_input, xi, adapted_sigma)
-    #     print("  [INFO] adaptive_smoothing -- " + str((k+1)/k_max*100) + "%")
-
-    img_input = euler_integration_test(img_input, xi, adapted_sigma)
+    img_input = euler_integration(img_input, xi, adapted_sigma)
 
     # print_debug(np.uint8(img_input))
     return img_input
 
-def laplacian(img):
+def laplacian_of_gaussian(img):
     img_grey = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(img_grey,(3,3),0)
     laplacian = cv2.Laplacian(blur,cv2.CV_64F)
@@ -267,19 +197,14 @@ def gradient(img):
     fx = cv2.filter2D(img_grey,-1,dx)
     fy = cv2.filter2D(img_grey,-1,dy)
 
-    # print_debug((abs(fx)+abs(fy))/2.0)
-
     return (abs(fx)+abs(fy))/2.0
-    
-    # img_grey = cv2.cvtColor(np.uint8(img),cv2.COLOR_RGB2GRAY)
-    # return (abs(cv2.Sobel(img_grey,cv2.CV_64F,0,1,ksize = 3)) + abs(cv2.Sobel(img_grey,cv2.CV_64F,1,0,ksize = 3)))/2.0
 
 def sharpening(img_input, nb_iter = 3):
     img = img_input.copy()
     img_new = np.zeros(img.shape, float)
 
     for k in range(0,nb_iter):
-        LoG_sign = np.sign(laplacian(np.uint8(img)))
+        LoG_sign = np.sign(laplacian_of_gaussian(np.uint8(img)))
         grad = gradient(img)
 
         height, width,_ = img.shape
@@ -296,33 +221,12 @@ def sharpening(img_input, nb_iter = 3):
         img = img_new.copy()
         print("   [INFO] sharpening -- "+str((k+1)/nb_iter*100) + "%")
 
-    # img_new = normalize_img(img_new)
-    # print_debug(img_new.astype(int))
-
     return img_new
-
-def merge(S1, S2):
-    #merge 2 structures tensor
-    E1 = S1[0]
-    F1 = S1[1]
-    G1 = S1[2]
-
-    E2 = S2[0]
-    F2 = S2[1]
-    G2 = S2[2]
-
-    result = S1.copy()
-    result[0] = (E1 + E2) / 2.0
-    result[1] = (F1 + F2) / 2.0
-    result[2] = (G1 + G2) / 2.0
-
-    return result
 
 def save_image(image, image_name):
     image = np.uint8(image)
-    # print_debug(image)
     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    cv2.imwrite('./image_result/result_'+image_name, image)
+    cv2.imwrite('./result_'+image_name, image)
 
 def show_image(image):
     #print the image
@@ -352,31 +256,30 @@ def print_eigen_vector(vector_field):
 
     plt.show()
 
-def print_lic(vector_field, length_pix = 10):
-    # fonctionne pas
-    resultat = lic.lic_flow(vector_field, length_pix)
-    print_debug(resultat[0])
-
 def main():
-    # image_name = 'dogs.jpg'
-    # image_name = 'chessboard.jpg'
+    # Fill with the name of the image
     image_name = 'lesinge.png'
+    # Choose the number of iteration
+    nb_iter = 3
+
     img = cv2.imread('./images/'+image_name)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
     result_img = img.copy()
 
-    for i in range(0,5):
+    for i in range(0,nb_iter):
         print("\n[INFO] Iteration "+ str(i+1))
 
         if(i != 0):
             img_gray = cv2.cvtColor(np.uint8(result_img),cv2.COLOR_RGB2GRAY)
             S_new = structure_tensor_calculation(img_gray)
             S = merge(S, S_new)
+            S = structure_tensor_smoothing(S)
             print("[INFO] Merge tensor structure -- done")
         else :
             img_gray = cv2.cvtColor(np.uint8(result_img),cv2.COLOR_RGB2GRAY)
             S = structure_tensor_calculation(img_gray)
+            S = structure_tensor_smoothing(S)
             print("[INFO] Compute tensor structure -- done")
 
         lambda1,lambda2 = compute_eigenvalue(S)
@@ -385,13 +288,10 @@ def main():
         eta, xi = compute_eigenvector(S,lambda1, lambda2)
         print("[INFO] Compute eigenvectors -- done")
 
-        # Debug -- Print eigen vector
         # print_eigen_vector(xi)
-        # print_lic(xi)
-        #-------------------------------
 
-        # result_img = adaptive_smoothing(result_img,lambda1,lambda2,xi,S, sigma= 6.0)
-        # print("[INFO] adaptive_smoothing -- done")
+        result_img = adaptive_smoothing(result_img,lambda1,lambda2,xi)
+        print("[INFO] adaptive_smoothing -- done")
 
         result_img = sharpening(result_img,1)
         print("[INFO] sharpening -- done")
@@ -406,8 +306,4 @@ main()
 
 stop = timeit.default_timer()
 
-print('Time :', stop - start, "s")  
-
-# TODO
-
-# continuer adaptive smoothing pour obtenir une image
+print('Time :', stop - start, "s")
